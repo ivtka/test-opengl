487054b560279d74b0c5f881273ef8590297b7c6
Ivan Tkachuk <ivtkac@gmail.com>
2021-12-25 19:16:50 +0200

    Clang-format

--- app/main.cpp
+++ app/main.cpp
@@ -1,20 +1,43 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <fstream>
 #include <iostream>
+#include <string>
 
-auto main() -> int {
+auto extractSource(const char *filename) -> std::string;
+
+auto main() -> int
+{
   if (!glfwInit()) {
     return EXIT_FAILURE;
   }
 
-  auto *window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+  glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+  glfwWindowHint(GLFW_SAMPLES, 8);
+
+  auto *window{ glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr) };
   if (!window) {
     glfwTerminate();
     return EXIT_FAILURE;
   }
 
   glfwMakeContextCurrent(window);
+
+  if (glewInit() != GLEW_OK) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  float positions[]{ -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f };
+
+  GLuint buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), positions, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
 
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -25,4 +48,15 @@ auto main() -> int {
   }
 
   return EXIT_SUCCESS;
+}
+
+auto extractSource(const char *filename) -> std::string
+{
+  auto shaderCode{ std::string{} };
+  auto shaderStream{ std::ifstream{ filename } };
+  if (shaderStream.is_open()) {
+    auto line{ std::string{} };
+  }
+
+  return shaderCode;
 }
